54fdedcd4f85ef5cd60ea7c55e1f5dfef3979c0b
Pawel Boguta <paul.boguta@gmail.com>
2025-09-26 13:53:03 +0200

    chore(web): swap process.env for t3 oss env

--- apps/web/instrumentation-client.ts
+++ apps/web/instrumentation-client.ts
@@ -1,6 +1,7 @@
 import posthog from "posthog-js";
+import { env } from "@/env";
 
-posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+posthog.init(env.NEXT_PUBLIC_POSTHOG_KEY, {
   api_host: "/ingest",
   ui_host: "https://us.posthog.com",
   defaults: "2025-05-24",

58b430d504dd9b6c4c159a173013ccf76bdf76db
Pawel Boguta <paul.boguta@gmail.com>
2025-09-26 13:35:28 +0200

    chore(web): add posthog for analytics and update dependencies

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -2,6 +2,20 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   typedRoutes: true,
+  // biome-ignore lint/suspicious/useAwait: posthog setup
+  async rewrites() {
+    return [
+      {
+        source: "/ingest/static/:path*",
+        destination: "https://us-assets.i.posthog.com/static/:path*",
+      },
+      {
+        source: "/ingest/:path*",
+        destination: "https://us.i.posthog.com/:path*",
+      },
+    ];
+  },
+  skipTrailingSlashRedirect: true,
 };
 
 export default nextConfig;

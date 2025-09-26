b93657949f7134cb6d41d03203d2992df1d136c2
Pawel Boguta <paul.boguta@gmail.com>
2025-09-26 12:58:23 +0200

    fix(api): update prisma client import to include .js extension

--- apps/server/src/db/index.ts
+++ apps/server/src/db/index.ts
@@ -1,5 +1,5 @@
-import { PrismaClient } from "../../prisma/generated/client";
 import { withAccelerate } from "@prisma/extension-accelerate";
+import { PrismaClient } from "../../prisma/generated/client.js";
 
 const prisma = new PrismaClient().$extends(withAccelerate());
 

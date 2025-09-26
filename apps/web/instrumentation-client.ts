c60ee11127328707d4036db7cd728d2dda3c0176
Pawel Boguta <paul.boguta@gmail.com>
2025-09-26 14:53:42 +0200

    chore(web): update posthog to EU region

--- apps/web/instrumentation-client.ts
+++ apps/web/instrumentation-client.ts
@@ -3,7 +3,7 @@ import { env } from "@/env";
 
 posthog.init(env.NEXT_PUBLIC_POSTHOG_KEY, {
   api_host: "/ingest",
-  ui_host: "https://us.posthog.com",
+  ui_host: "https://eu.posthog.com",
   defaults: "2025-05-24",
   capture_exceptions: false,
 });
--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -7,11 +7,11 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/ingest/static/:path*",
-        destination: "https://us-assets.i.posthog.com/static/:path*",
+        destination: "https://eu-assets.i.posthog.com/static/:path*",
       },
       {
         source: "/ingest/:path*",
-        destination: "https://us.i.posthog.com/:path*",
+        destination: "https://eu.i.posthog.com/:path*",
       },
     ];
   },
